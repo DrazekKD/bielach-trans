47e96470a6136d25a457728eef010a4c57ece03a
Kamil Drążkiewicz <k.drazkiewicz@getprintbox.com>
2019-03-26 18:19:41 +0100

    add loader

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,8 @@
-// //preloader
-// $(window).on('load', function () {
-// $(".preloader-logo").addClass('fullSize')
-// 	$("#preloader").fadeOut();
-// 	setTimeout(function() {
-// 		$("#preloader").remove();
-// 	}, 500);
-// });
+//preloader
+$(window).on('load', function () {
+$(".preloader-logo").addClass('fullSize')
+	$("#preloader").fadeOut();
+	setTimeout(function() {
+		$("#preloader").remove();
+	}, 500);
+});
